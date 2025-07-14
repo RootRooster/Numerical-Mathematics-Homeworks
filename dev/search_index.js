78774c238d0ba5ce65eb96a83e32c7e0c2268c94
Documenter.jl <documenter@juliadocs.github.io>
2025-07-14 20:04:05 +0000

    build based on 654e5ac

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Homeworks.jl","page":"Overview","title":"Homeworks.jl","text":"","category":"section"},{"location":"","page":"Overview","title":"Overview","text":"Documentation for Homeworks.jl","category":"page"}]
+}
