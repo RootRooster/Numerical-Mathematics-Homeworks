26bdd2a5374d906051bf52c98042b623dc91fc19
Documenter.jl <documenter@juliadocs.github.io>
2025-07-14 20:17:26 +0000

    build based on 9156161

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Homeworks.jl","page":"Overview","title":"Homeworks.jl","text":"","category":"section"},{"location":"","page":"Overview","title":"Overview","text":"Documentation for Homeworks.jl","category":"page"}]
+[{"location":"#Homeworks.jl","page":"Overview","title":"Homeworks.jl","text":"","category":"section"},{"location":"","page":"Overview","title":"Overview","text":"Documentation for Homeworks.jl","category":"page"},{"location":"#Homeworks.greet-Tuple{}","page":"Overview","title":"Homeworks.greet","text":"greet(x)\n\nReturns hello world!\n\n\n\n\n\n","category":"method"}]
 }
